011754b6912a55396354a85425dba30a6ff26fdf
Saadia Saleem <154665738+saadiasaleem67@users.noreply.github.com>
2024-02-28 23:49:20 +0500

    Add files via upload

---
+++ quiz.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer"
+const quiz:string="https://opentdb.com/api.php?amount=6&category=21&difficulty=easy&type=multiple"
+
+let fetchdata = async (data:string)=> {
+    let quizdta:any=await fetch(data)
+    let res = await quizdta.json()
+    return res.results;
+     
+}
+let data = await fetchdata(quiz)
+
+let startQuiz= async () => {
+    let score:number = 0
+    let Name = await inquirer.prompt({
+        type:"input",
+        name:"user" , 
+        message:"Enter your Name:"
+    })
+    for(let i=1 ; i<=5 ; i++){
+        let answer = [...data[i].incorrect_answers,data[i].correct_answer];
+
+        let ans = await inquirer.prompt({
+            type:"list",
+            name:"quiz",
+            message:data[i].question,
+            choices:answer.map((val:any)=>val),
+        })
+        if(ans.quiz == data[i].correct_answer){
+            ++score
+            console.log("Correct");  
+        }
+        else{
+            console.log(`Wrong! the answer is ${data[i].correct_answer}`); 
+        }
+    }
+    console.log(`Dear ${Name.user} , your score is ${score} out of 5`);
+} 
+startQuiz()
